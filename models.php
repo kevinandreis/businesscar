cd03c13dc5c5711b318e11473827edbd7a3edf27
kevinandreis <kevinandrei2321@gmail.com>
2024-11-04 06:59:48 +0800

    Update models.php

--- models.php
+++ models.php
@@ -1,47 +1,31 @@
 <?php
-require_once 'dbConfig.php';
 
-function insertIntoCarRecords($pdo, $make, $model, $year, $license_plate) {
-    $sql = "INSERT INTO Cars (Make, Model, Year, LicensePlate) VALUES (?, ?, ?, ?)";
-    $stmt = $pdo->prepare($sql);
+function seeAllCars($pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM car");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function addCar($pdo, $make, $model, $year, $license_plate) {
+    $stmt = $pdo->prepare("INSERT INTO car (make, model, year, license_plate) VALUES (?, ?, ?, ?)");
     return $stmt->execute([$make, $model, $year, $license_plate]);
 }
 
-function seeAllCarRecords($pdo) {
-    $sql = "SELECT * FROM Cars";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    return $stmt->fetchAll();
-}
 
-function getCarByID($pdo, $car_id) {
-    $sql = "SELECT * FROM Cars WHERE CarID = ?";
-    $stmt = $pdo->prepare($sql);
-    return $stmt->execute([$car_id]) ? $stmt->fetch() : null;
-}
-
-function updateCar($pdo, $car_id, $make, $model, $year, $license_plate) {
-    $sql = "UPDATE Cars SET Make = ?, Model = ?, Year = ?, LicensePlate = ? WHERE CarID = ?";
-    $stmt = $pdo->prepare($sql);
-    return $stmt->execute([$make, $model, $year, $license_plate, $car_id]);
+function getCarById($pdo, $car_id) {
+    $stmt = $pdo->prepare("SELECT * FROM car WHERE car_id = ?");
+    $stmt->execute([$car_id]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 function deleteCar($pdo, $car_id) {
-    $sql = "DELETE FROM Cars WHERE CarID = ?";
-    $stmt = $pdo->prepare($sql);
+    $stmt = $pdo->prepare("DELETE FROM car WHERE car_id = ?");
     return $stmt->execute([$car_id]);
 }
 
-// Functions for services management
-function insertService($pdo, $car_id, $service_type, $service_date, $service_cost) {
-    $sql = "INSERT INTO Services (CarID, ServiceType, ServiceDate, ServiceCost) VALUES (?, ?, ?, ?)";
-    $stmt = $pdo->prepare($sql);
-    return $stmt->execute([$car_id, $service_type, $service_date, $service_cost]);
+function updateCar($pdo, $car_id, $make, $model, $year, $license_plate) {
+    $stmt = $pdo->prepare("UPDATE car SET make = ?, model = ?, year = ?, license_plate = ? WHERE car_id = ?");
+    return $stmt->execute([$make, $model, $year, $license_plate, $car_id]);
 }
 
-function seeAllServicesForCar($pdo, $car_id) {
-    $sql = "SELECT * FROM Services WHERE CarID = ?";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$car_id]);
-    return $stmt->fetchAll();
-}
+?>
