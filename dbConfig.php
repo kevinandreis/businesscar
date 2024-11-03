53dd9de7cd336bc7ae25d40cc95492b6fbbfd990
kevinandreis <kevinandrei2321@gmail.com>
2024-11-03 17:44:30 +0800

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,10 +1,18 @@
-dbConfig.php
-
 <?php  
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "SALAZAR";
-$dsn = "mysql:host={$host};dbname={$dbname}";
-$pdo = new PDO($dsn, $user, $password);
+$host = "localhost"; 
+$user = "root";       
+$password = "";       
+$dbname = "car"; 
+
+
+$dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+
+try {
+
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit(); 
+}
 ?>
